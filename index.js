ec543e3c334e848795cde98797915c3a207063c9
RaildoBru <raildobruno@gmail.com>
2025-02-18 01:11:16 -0300

    adicionando swagger

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./db-config.js";
 import productRoutes  from "./src/routes/product.routes.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./src/docs/swagger.js";
 
 dotenv.config();
 const app = express();
@@ -11,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/products", productRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
